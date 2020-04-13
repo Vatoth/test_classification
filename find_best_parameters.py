cff2d956ab7d6cb8bd207bfb8be88016422201bc
Quentin Sonnefraud <quentin.sonnefraud@protonmail.com>
2020-04-13 01:44:15 +0100

    [MODIFY]: clear variable name

--- find_best_parameters.py
+++ find_best_parameters.py
@@ -4,23 +4,23 @@ from sklearn.metrics import accuracy_score
 
 
 def find_best_parameters(
-        c_parameter_name,
-        c_parameter_values,
+        parameter_name,
+        parameter_values,
         x_train,
         x_test,
         y_train,
         y_test):
 
-    df = pd.DataFrame(columns=[c_parameter_name, 'accuracy'])
+    df = pd.DataFrame(columns=[parameter_name, 'accuracy'])
 
-    for input_parameter in c_parameter_values:
+    for parameter_value in parameter_values:
         clf = DecisionTreeClassifier(
-            **{'random_state': 42, c_parameter_name: input_parameter})
+            **{'random_state': 42, parameter_name: parameter_value})
         clf = clf.fit(x_train, y_train)
         y_pred = clf.predict(x_test)
         acc_score = accuracy_score(y_test, y_pred) * 100
         df = df.append(
-            {c_parameter_name: input_parameter, 'accuracy': acc_score},
+            {parameter_name: parameter_value, 'accuracy': acc_score},
             ignore_index=True)
     best = df.loc[df['accuracy'].idxmax()]
-    return best[c_parameter_name], best['accuracy']
+    return best[parameter_name], best['accuracy']
