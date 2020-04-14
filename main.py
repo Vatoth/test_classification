73f5a14c1ce8cab96a1ac53d13537fce69d89bbb
Quentin Sonnefraud <quentin.sonnefraud@protonmail.com>
2020-04-14 14:24:22 +0100

    [ADD]: plots

--- main.py
+++ main.py
@@ -4,18 +4,14 @@
 from scipy.io import arff
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
+from pandas.plotting import scatter_matrix
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 from find_best_parameters import find_best_parameters
 from sklearn.model_selection._validation import validation_curve
-
-
-X = None
-Y = None
-
 
 def plot_balance_class(classes):
     """Plot balance between classes
@@ -33,7 +29,6 @@ def load_data(dataset_path: str):
     """
     data = arff.loadarff(dataset_path)
     data_frame = pd.DataFrame(data[0])
-    data_frame['class'] = data_frame['class'].astype(str)
     return data_frame
 
 
@@ -41,7 +36,7 @@ def plot_validation_curve(X, Y, param_name, param_range):
     train_scores, test_scores = validation_curve(
         DecisionTreeClassifier(),
         X, Y, param_range=param_range, param_name=param_name, cv=5,
-        scoring="accuracy", n_jobs=8)
+        scoring="accuracy", n_jobs=-1)
     train_mean = np.mean(train_scores, axis=1)
     train_std = np.std(train_scores, axis=1)
     test_mean = np.mean(test_scores, axis=1)
@@ -120,21 +115,37 @@ def find_best_classifier(x_train, x_test, y_train, y_test):
     clf = clf.fit(x_train, y_train)
     return clf
 
-def search_with_grid(X, Y, x_train, x_test, y_train, y_test):
+def search_with_grid(x_train, x_test, y_train, y_test):
     param_grid = {
+        'criterion': ['gini', 'entropy'],
         'max_depth': np.arange(1, 15),
-        'min_samples_split': np.arange(2, 100),
-        'min_samples_leaf': np.arange(2, 150),
-        'max_leaf_nodes': np.arange(2, 100),
-        'min_impurity_decrease': np.arange(0.0005, 0.003, 0.0005),
+        'min_samples_split': np.arange(2, 100, 2),
+        'min_samples_leaf': np.arange(2, 150, 5),
+        'max_leaf_nodes': np.arange(2, 100, 5),
+        #'min_impurity_decrease': np.arange(0.0005, 0.003, 0.0005),
     }
-    grid = GridSearchCV(DecisionTreeClassifier(), param_grid, cv=5, n_jobs=-1)
-    grid.fit(X, Y)
+    print("Commencing grid search")
+    grid = GridSearchCV(DecisionTreeClassifier(), param_grid, cv=5, n_jobs=-1, verbose=1)
+    grid.fit(x_train, y_train)
     clf = DecisionTreeClassifier(**grid.best_params_)
     clf.fit(x_train, y_train)
     y_pred = clf.predict(x_test)
     acc_score = accuracy_score(y_test, y_pred) * 100
-    print("Accuracy after parameters tunning with grid search:", acc_score, grid.best_params_, grid.best_score_, grid.best_index_)
+    print("Accuracy after parameters tunning with grid search:",  acc_score, grid.best_params_, grid.best_score_, grid.best_index_)
+    print(classification_report(y_test, y_pred, target_names=[1, 0]))
+
+def features_analysis(data_frame):
+    tested_positive = data_frame[data_frame['class'] == 1]
+    tested_positive.hist(figsize=(15,10))
+    plt.savefig('features_repartition_positive' + '.png')
+    plt.clf()
+    tested_negative = data_frame[data_frame['class'] == 0]
+    tested_negative.hist(figsize=(15,10))
+    plt.savefig('features_repartition_negative' + '.png')
+    plt.clf()
+    scatter_matrix(data_frame, figsize=(15, 10))
+    plt.savefig('correlation' + '.png')
+    plt.clf()
 
 
 def main():
@@ -143,6 +154,8 @@ def main():
     data_frame = load_data('diabetes.arff')
     features_cols = [i for i in data_frame.columns.values.tolist() if i not in [
         'class']]
+    features_analysis(data_frame)
+    exit(1)
     X, Y = data_frame[features_cols], data_frame['class']
     plot_validation_curve(X, Y, 'max_depth', np.arange(1, 30))
     plot_validation_curve(X, Y, 'min_samples_split', np.arange(2, 200))
@@ -152,13 +165,13 @@ def main():
         X, Y, 'min_impurity_decrease', np.arange(
             0.0005, 0.1, 0.0005))
     x_train, x_test, y_train, y_test = train_test_split(
-        X, Y, test_size=0.20, random_state=42)
+        X, Y, test_size=0.25, random_state=42)
     clf = DecisionTreeClassifier(random_state=42)
     clf = clf.fit(x_train, y_train)
     y_pred = clf.predict(x_test)
     acc_score = accuracy_score(y_test, y_pred) * 100
     print("Accuracy before parameters tunning:", acc_score)
-    search_with_grid(X, Y, x_train, x_test, y_train, y_test)
+    #search_with_grid(x_train, x_test, y_train, y_test)
     clf = find_best_classifier(x_train, x_test, y_train, y_test)
     clf.fit(x_train, y_train)
     y_pred = clf.predict(x_test)
